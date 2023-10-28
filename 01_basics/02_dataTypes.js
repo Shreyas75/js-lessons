413a77befd10e873bcf742cc464d4ade58f10881
Shreyas Manoti <68011587+Shreyas75@users.noreply.github.com>
2023-10-28 16:43:05 +0000

    two more updated

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //Treat all JS Code as Newer Version
+
+// alert(3+3) //We are using Nodejs, not browser
+
+
+
+let name = "Shreyas";
+let add = 18;
+let isLoggedIn = true;
+let state = null; 
+
+/* Data Types
+ # PRIMITIVE DATA TYPES:
+    1. Number
+    2. bigint
+    3. string => ""
+    4. boolean => true/false
+    5. null => standalone value
+    6. undefined => 
+    7. symbol => unique
+
+ #OBJECT
+    1.
+*/
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "hitesh";
+
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+/*
+    CONVERT TO NUMBER
+  "33" => 33
+  "33abc" => NaN
+  true => 1; false => 0
+*/
+
+let isLoggedIn = "shryeas";
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolIsLoggedIn)
+
+/*
+    CONVERT TO BOOLEAN
+  1 => true; 0 => false
+  "" => false
+  "shreyas" => true
+*/
+
+
+let someNumber = 32;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
