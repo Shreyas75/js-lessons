8fcd3a9e7b29653533c6c97512f8bc1c6fca7464
Shreyas Manoti <68011587+Shreyas75@users.noreply.github.com>
2023-10-29 11:41:17 +0000

    new uploads

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,37 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// *****************************************************
+
+// 2 TYPES OF MEMORY: STACK & HEAP
+/*
+    Stack => (Primitive Datatypes)
+    Heap => (Non-Primitive Datatypes)
+*/
+
+let myYoutubeName = "shreyasmanoti";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode"
+
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+// userTwo.email = "changed"
+
+console.log(userTwo)
+console.log(userOne)
+
+
+
