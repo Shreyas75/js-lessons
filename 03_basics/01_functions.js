9983f118e258a5272b2fd5c3e8d71fef86f8c947
Shreyas Manoti <68011587+Shreyas75@users.noreply.github.com>
2023-10-31 00:01:16 +0000

    func

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+function sayMyName() {
+    console.log("S")
+    console.log("H")
+    console.log("R")
+    console.log("E")
+    console.log("Y")
+    console.log("A")
+    console.log("S")
+}
+
+// sayMyName
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    return (number1 + number2);
+}
+
+
+
+const result = addTwoNumbers(5,3)
+
+console.log("Result: ", result);
